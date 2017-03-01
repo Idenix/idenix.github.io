21e056cb459015b19d2c4b715fcc579e978e1f61
Elyam Millimono <Elyam Millimono>
2017-03-01 20:54:11 +0100

    Supressed the dog image to put it as background image. Also moved the "style.css" file to the first folder.

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,12 @@ if(!localStorage.getItem('nom')) {
 monBouton.onclick = function() {
   définirNomUtilisateur();
 }
+
+var Bouton = document.querySelector('img')
+function test() {
+	var test = 'Bonjour';
+	alert(test);
+}
+Bouton.onclick = function() {
+	test()
+}
