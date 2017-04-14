c078d74171fa18d8668ba579a2bde05a41eb40b5
Idenix <elyam.millimono@gmail.com>
2017-04-14 20:30:54 +0200

    des trucs

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,24 @@
 var monBouton = document.querySelector('button');
+
 var monTitre = document.querySelector('h1');
+
 function définirNomUtilisateur() {
   var monNom = prompt("C'est quoi ton pseudal ?");
   localStorage.setItem('nom', monNom);
   monTitre.textContent = 'Bienvenue, ' + monNom;
 }
+
 if(!localStorage.getItem('nom')) {
   définirNomUtilisateur();
 } else {
   var nomEnregistré = localStorage.getItem('nom');
   monTitre.textContent = 'Bienvenue, ' + nomEnregistré;
 }
+
+if (nomEnregistré == "je t'aime trop Idenix") {
+		alert ("Moi aussi je t'aime...")
+	}
+
 monBouton.onclick = function() {
   définirNomUtilisateur();
 }
